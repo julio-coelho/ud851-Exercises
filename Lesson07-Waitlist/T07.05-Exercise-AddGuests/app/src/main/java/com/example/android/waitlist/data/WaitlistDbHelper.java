8817785e86a571f17316ee98fa734bda51e5fea3
Julio Coelho <juliocoelho@icloud.com>
2018-06-03 14:21:14 -0300

    16. Exercise: Add Guests

--- Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
+++ Lesson07-Waitlist/T07.05-Exercise-AddGuests/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -13,7 +13,7 @@ public class WaitlistDbHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "waitlist.db";
 
     // If you change the database schema, you must increment the database version
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     // Constructor
     public WaitlistDbHelper(Context context) {
